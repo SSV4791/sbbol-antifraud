bcb10421d4e3d54d759cc0c70b38e6141c11c34b
Писарев Филипп Михайлович <fmpisarev@sberbank.ru>
2021-09-07 13:02:31 +0300

    DCBEFSMSC5-765 Изменить формат полей LocalDateTime, отправляемых в ФП ИС
    * в поле timeOfOccurence, миллисекунды отображаются с точностью до тысячных
    * в поле timeStamp, миллисекунды отображаются с точностью до тысячных
    * исправлен конфиг ef-ingress-healthcheck

--- dataspace-antifraud/antifraud-api/src/main/java/ru/sberbank/pprb/sbbol/antifraud/api/analyze/payment/request/EventDataHeader.java
+++ dataspace-antifraud/antifraud-api/src/main/java/ru/sberbank/pprb/sbbol/antifraud/api/analyze/payment/request/EventDataHeader.java
@@ -1,6 +1,7 @@
 package ru.sberbank.pprb.sbbol.antifraud.api.analyze.payment.request;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
@@ -35,6 +36,7 @@ public class EventDataHeader implements Serializable {
      */
     @JsonSerialize(using = LocalDateTimeSerializer.class)
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
     private LocalDateTime timeOfOccurrence;
 
     @JsonCreator
