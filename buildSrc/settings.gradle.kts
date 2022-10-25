45add02458b9ad523444118196176a8a2b2f3efe
Писарев Филипп Михайлович <fmpisarev@sberbank.ru>
2022-10-25 15:19:40 +0300

    DCBEFSMSC5-1891 Миграция с Nexus 2 на Nexus 3. Этап 2

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,7 +1,13 @@
 pluginManagement {
     repositories {
         maven {
-            url = uri("https://nexus.sigma.sbrf.ru/nexus/content/groups/public/")
+            url = uri("https://nexus-ci.delta.sbrf.ru/repository/public/")
+            val tokenName: String by settings
+            val tokenPassword: String by settings
+            credentials {
+                username = tokenName
+                password = tokenPassword
+            }
             isAllowInsecureProtocol = true
         }
     }
